b9cd2d2e34220d383c9aa5e9d0a82c9229e6e904
George Dewar <george@dewar.co.nz>
2024-11-16 22:07:54 +1300

    POC rendering an image for a certain block type

--- tools/render_castle_graphics.py
+++ tools/render_castle_graphics.py
@@ -15,6 +15,8 @@ blocks_len = blocks_width * blocks_height
 block_width=16
 block_height=16
 
+img20 = cv2.imread("extracted/images/red_block.png")
+
 with open(castle_structure_filename, "rb") as input_file:
     # Read the castle data
     data = input_file.read()
@@ -28,7 +30,9 @@ with open(castle_structure_filename, "rb") as input_file:
             byte = data[input_idx]
             input_idx += 1
 
-            if byte > 1:
+            if byte == 0x20:
+                img[x*block_width:x*block_width+block_width, y*block_height:y*block_height+block_height] = img20
+            elif byte > 1:
                 for block_x in range(block_height):
                     for block_y in range(block_width):
                         img[x*block_width + block_x,y*block_height + block_y] = [255,255,255]
@@ -36,6 +40,7 @@ with open(castle_structure_filename, "rb") as input_file:
     cv2.imwrite(output_filename, img)
 
     # Show the image
-    preview = cv2.resize(img, (blocks_width * 4, blocks_height * 4))
-    cv2.imshow("Castle", preview)
+    #preview = cv2.resize(img, (blocks_width * 4, blocks_height * 4))
+    cv2.namedWindow("output", cv2.WINDOW_NORMAL)    # Create window with freedom of dimensions
+    cv2.imshow("output", img)
     cv2.waitKey(0)
