2a7c8dfbee23ed36c7237f8e45a8e5117a297142
George Dewar <george@dewar.co.nz>
2024-11-20 23:14:34 +1300

    Refactor render_castle_graphics.py

--- tools/render_castle_graphics.py
+++ tools/render_castle_graphics.py
@@ -5,6 +5,7 @@ import cv2
 castle_num = sys.argv[1]
 castle_structure_filename = f"extracted/castle{castle_num}_structure.bin"
 castle_items_filename = f"extracted/castle{castle_num}_items.bin"
+castle_magic_filename = f"extracted/castle{castle_num}_unknown.bin"
 output_filename = f"render/castle{castle_num}.png"
 
 # Dimensions of the castle in blocks
@@ -24,34 +25,44 @@ items = {}
 for i in range(240):
     items[i] = cv2.imread(f"extracted/images/items/{i:02x}.png")
 
+# Read the castle data
 with open(castle_structure_filename, "rb") as structure_file:
-    with open(castle_items_filename, "rb") as items_file:
-        # Read the castle data
-        structure_data = structure_file.read()
-        items_data = items_file.read()
-        input_idx = 0
+    structure_data = structure_file.read()
+with open(castle_items_filename, "rb") as items_file:
+    items_data = items_file.read()
+with open(castle_magic_filename, "rb") as magic_file:
+    magic_data = magic_file.read()
 
-        # create the data behind our output image
-        img = np.zeros((blocks_height * block_height, blocks_width * block_width, 3),np.uint8)
+# Keep track of where we are - same index for all three files
+input_idx = 0
 
-        for x in range(blocks_height):
-            for y in range(blocks_width):
-                # Draw items
-                byte = items_data[input_idx]
-                if byte != 0:
-                    img[x*block_width:x*block_width+block_width, y*block_height:y*block_height+block_height] = items[byte]
+# create the data behind our output image
+img = np.zeros((blocks_height * block_height, blocks_width * block_width, 3),np.uint8)
 
-                # Draw structure
-                byte = structure_data[input_idx]
-                if byte != 0:
-                    img[x*block_width:x*block_width+block_width, y*block_height:y*block_height+block_height] = structure[byte]
+for x in range(blocks_height):
+    for y in range(blocks_width):
+        # Draw items
+        byte = items_data[input_idx]
+        if byte != 0:
+            img[x*block_width:x*block_width+block_width, y*block_height:y*block_height+block_height] = items[byte]
 
-                input_idx += 1
+        # Draw structure
+        byte = structure_data[input_idx]
+        if byte != 0:
+            img[x*block_width:x*block_width+block_width, y*block_height:y*block_height+block_height] = structure[byte]
 
-        cv2.imwrite(output_filename, img)
+        input_idx += 1
 
-        # Show the image
-        #preview = cv2.resize(img, (blocks_width * 4, blocks_height * 4))
-        cv2.namedWindow("output", cv2.WINDOW_NORMAL)    # Create window with freedom of dimensions
-        cv2.imshow("output", img)
-        cv2.waitKey(0)
+cv2.imwrite(output_filename, img)
+
+# Show the image
+cv2.namedWindow("output", cv2.WINDOW_NORMAL)    # Create window with freedom of dimensions
+cv2.imshow("output", img)
+
+# Wait for 'q' key or window closed
+wait_time = 100
+while cv2.getWindowProperty('output', cv2.WND_PROP_VISIBLE) >= 1:
+    keyCode = cv2.waitKey(wait_time)
+    if (keyCode & 0xFF) == ord("q"):
+        cv2.destroyAllWindows()
+        break
