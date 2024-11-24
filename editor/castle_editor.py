f1fed808c61d15c31d537cc273403dca7f7ef6c8
George Dewar <george@dewar.co.nz>
2024-11-25 00:00:02 +1300

    Show available structure and items

--- editor/castle_editor.py
+++ editor/castle_editor.py
@@ -9,9 +9,23 @@ import numpy as np
 HOME=".."
 castle_num = 1
 
-available_items = {
-    ""
-}
+# Define structures as 0-199, minus certain ranges of animation sprites
+available_structure = range(200)
+ranges_to_remove = [(33, 42), (44, 47), (49,52), (97,107), (114,119), (121,123), (124,129), (137,143), (158,163), (178,183), (185,185), (198,199)]
+available_structure = [
+    num for num in available_structure 
+    if not any(start <= num <= end for start, end in ranges_to_remove)
+]
+
+# Define items as 0-192, minus certain ranges of animation sprites
+available_items = range(193)
+ranges_to_remove = [(8,14), (19,25), (28,34), (38,44), (47,53), (55,58), (60,63), (65,76), (78,80), (82,84), (86,88), (90,92), (94,97), (106,109), (111,114), (116,119), (121,130), (132,133), (135,136), (138,139), (141,142), (145,160), (162,165), (167,168), (170,173), (175,176), (178,181), (183,184), (186,189), (191,192)]
+available_items = [
+    num for num in available_items 
+    if not any(start <= num <= end for start, end in ranges_to_remove)
+]
+
+RIGHT_MENU_SIZE = 500
 
 class CastleEditorWindow(QtWidgets.QWidget):
     def __init__(self):
@@ -61,11 +75,46 @@ class CastleEditorWindow(QtWidgets.QWidget):
         self.selectedCellInfoLayout.addLayout(self.selectedCellStructureLayout)
         self.selectedCellInfoLayout.addLayout(self.selectedCellMagicLayout)
 
+        self.structure_sprites = {}
+        for i in range(200):
+            self.structure_sprites[i] = cv2.imread(f"{HOME}/extracted/images/structure/{i:03}.png")
+
+        self.item_sprites = {}
+        for i in range(200):
+            self.item_sprites[i] = cv2.imread(f"{HOME}/extracted/images/items/{i:03}.png")
+        
+        self.structureLayout = QtWidgets.QGridLayout()
+        GRID_SPACING = 5
+        self.structureLayout.setSpacing(GRID_SPACING)
+        for idx, structure in enumerate(available_structure):
+            label = QtWidgets.QLabel()
+            sprite = self.structure_sprites[structure]
+            tileSize = 32
+            qimage = QtGui.QImage(sprite.data, sprite.shape[1], sprite.shape[0], QtGui.QImage.Format_RGB888).rgbSwapped().scaled(tileSize, tileSize, QtCore.Qt.AspectRatioMode.KeepAspectRatio)
+            label.setPixmap(QtGui.QPixmap.fromImage(qimage))
+            num_cols = (RIGHT_MENU_SIZE - 20) // (tileSize + 5)
+            self.structureLayout.addWidget(label, idx // num_cols, idx % num_cols)
+        
+        self.itemsLayout = QtWidgets.QGridLayout()
+        self.itemsLayout.setSpacing(GRID_SPACING)
+        for idx, item in enumerate(available_items):
+            label = QtWidgets.QLabel()
+            sprite = self.item_sprites[item]
+            tileSize = 32
+            qimage = QtGui.QImage(sprite.data, sprite.shape[1], sprite.shape[0], QtGui.QImage.Format_RGB888).rgbSwapped().scaled(tileSize, tileSize, QtCore.Qt.AspectRatioMode.KeepAspectRatio)
+            label.setPixmap(QtGui.QPixmap.fromImage(qimage))
+            num_cols = (RIGHT_MENU_SIZE - 20) // (tileSize + 5)
+            self.itemsLayout.addWidget(label, idx // num_cols, idx % num_cols)
+
         rightMenuLayout = QtWidgets.QWidget()
-        rightMenuLayout.setFixedWidth(250)
+        rightMenuLayout.setFixedWidth(RIGHT_MENU_SIZE)
         self.rightMenu = QtWidgets.QVBoxLayout()
         self.rightMenu.addWidget(self.selectedCellLabel)
         self.rightMenu.addLayout(self.selectedCellInfoLayout)
+        self.rightMenu.addWidget(QtWidgets.QLabel(text = "Structure:"))
+        self.rightMenu.addLayout(self.structureLayout)
+        self.rightMenu.addWidget(QtWidgets.QLabel(text = "Items:"))
+        self.rightMenu.addLayout(self.itemsLayout)
         self.rightMenu.addStretch()
         self.rightMenu.addWidget(self.gameCoords)
         self.rightMenu.addWidget(self.labelCoords)
